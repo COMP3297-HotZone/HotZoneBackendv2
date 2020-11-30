785d0b972987f83f9a9801d7b5ff1eb67c497f65
Derrick Andry <derrickandry@gmail.com>
2020-12-01 00:24:12 +0800

    fixed static root bug

--- hotzone_config/settings.py
+++ hotzone_config/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
